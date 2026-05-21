5f019eab7df833db18842f06e9234447ed2dbf87
jorlando989 <jorlando989@gmail.com>
2026-05-21 15:26:32 -0400

    implement collision for shots and splitting

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 from circleshape import *
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x: float, y: float, radius: float) -> None:
@@ -10,3 +12,20 @@ class Asteroid(CircleShape):
 
     def update(self, dt: float) -> None:
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        new_angle = random.uniform(20,50)
+        vector1 = self.velocity.rotate(new_angle)
+        vector2 = self.velocity.rotate(-1 * new_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = vector1 * 1.2
+
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2.velocity = vector2 * 1.2
+
